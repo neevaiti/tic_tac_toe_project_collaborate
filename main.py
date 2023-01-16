0fc1c52fd9407d923eb6bade70058192affbdac1
Ant <antonin.lemullois@pm.me>
2023-01-16 13:58:00 +0100

    jouer coup

--- main.py
+++ main.py
@@ -16,3 +16,12 @@ def afficher_grille(plateau:dict) -> None:
             else:
                 print(elt, end="\t|\t")
         print("\n---------------------------------------------------------")
+
+def jouer_coup(plateau:dict, joueur:str, coup:str) -> None:
+    """Fonction qui joue un coup (Ne vérifie pas si le coup est valide)
+    Args:
+        plateau (dict): Le plateau de jeu
+        joueur (str): "O" ou "X"
+        coup (str): Coordonnées de la forme "A1"
+    """
+    plateau[coup[0].upper()][int(coup[1])] = joueur
